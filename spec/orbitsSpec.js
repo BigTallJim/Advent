8e78b5fcaff8936ff88bee03b0fb7dd47a762153
James Holton <james.holton@yahoo.co.uk>
2019-12-18 15:19:44 +0000

    Created first indirect orbit calc

--- spec/orbitsSpec.js
+++ spec/orbitsSpec.js
@@ -1,7 +1,16 @@
 describe('UniversalOrbit', function(){
   it("has 1 orbit when B orbits COM", function(){
     let orbits = new UniversalOrbit();
-    // console.log(orbits)
     expect(orbits.count('COM)B')).toEqual(1);
   });
+
+  it("has 2 orbit when B orbits COM and C Orbits B", function(){
+    let orbits = new UniversalOrbit();
+    expect(orbits.count('COM)B B)C')).toEqual(3);
+  });
+
+  // it("has 3 orbit when B orbits COM and C Orbits B and D Orbits C", function(){
+  //   let orbits = new UniversalOrbit();
+  //   expect(orbits.count('COM)B B)C C)D')).toEqual(6);
+  // });
 });
--- src/D6-orbits.js
+++ src/D6-orbits.js
@@ -2,5 +2,27 @@ function UniversalOrbit(){
 }
 
 UniversalOrbit.prototype.count = function(orbits){
-    return 1
+    let pairs = orbits.split(" ");
+    let orbitsMap = new Map();
+    let directCount = 0;
+    let indirectCount = 0;
+
+    console.log(pairs);
+    pairs.forEach(function(pair){
+        let parentPlanet = pair.split(')')[0];
+        let  planet = pair.split(')')[1];
+        orbitsMap.set(planet, parentPlanet);
+    });
+
+    orbitsMap.forEach(function(pair){
+        console.log(pair);
+        if (orbitsMap.has(pair)){
+            indirectCount++;
+        }
+    });
+
+    directCount = orbitsMap.size;
+
+    return directCount + indirectCount;
 }
+
